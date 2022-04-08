157ec985a9155f89c5b8dcdbbd9ea095e60ebcf5
JC Flores <tothenextcode@gmail.com>
2022-04-08 14:05:48 +0000

    Implemented recursively summing array numbers and gitignored package-lock.json

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+
+  for (const item of array) {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+
+  return sum;
 }
 
 module.exports = sumItems;
